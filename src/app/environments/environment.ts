5e4152d19bcb4b2692b281548a59e45a6b1b0d16
pablomendoza76 <116389858+pablomendoza76@users.noreply.github.com>
2026-01-27 10:24:34 -0500

    ajustes visaules en el chatbot

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
   supabaseUrl: 'https://mjqrcgfgwbqkikzzymwl.supabase.co',
 
   // CLAVE PUBLICABLE (SEGURA PARA FRONTEND)
-  supabaseAnonKey: '1'
+  supabaseAnonKey: 'sb_publicable_qcCUlHxa9P13ezTcMTR7QQ_W9Mb7seR'
 };
